a697ea0853ab4dc50ad7446c5db87404f3ef6fc6
Alex Paul <lexypaul13@yahoo.com>
2021-05-31 09:08:49 -0400

    Fixed Json Output to console

--- Memecoins/MemecoinsVC.swift
+++ Memecoins/MemecoinsVC.swift
@@ -11,12 +11,13 @@ class MemecoinsVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var memecoins = [String:Any]()
-    let memesData = [String:MemecoinData]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.backgroundColor = .black
+
         getMemecoins()
         
     }
@@ -31,19 +32,15 @@ class MemecoinsVC: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
-//    func memes(){
-//        memesData.forEach { (key: String, value: MemecoinData) in
-//            memecoins[key] = value
-//        }
-//    }
-//
+
+
     func getMemecoins(){
         NetworkManger.shared.get(.tokens, urlString: "") { [weak self] (result: Result<[String:Any]?, ErroMessage> ) in
             guard let self = self else { return }
             switch result{
-            case .success(_):
-                self.memecoins = self.memesData
-                print(self.memecoins)
+            case .success(let memecoins):
+                self.memecoins = memecoins ?? [:]
+               // print(self.memecoins)
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -61,13 +58,21 @@ class MemecoinsVC: UIViewController {
 extension MemecoinsVC: UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return memesData.count
+        return memecoins.count
+    }
+   
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       return 163
+   }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "memes", for: indexPath) as! MemecoinsTableViewCell
         
-        for (_, value) in memesData{
+        for (_, value) in memecoins{
             cell.coinName.text = value.name
             cell.memeprice.text = value.price
             cell.tickerSymbol.text = value.symbol
